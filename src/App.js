ef4b03cbc5f38904beacc5ffe9a5be5d76486ac4
xin.hu <xin.hu@xinhus-MBP.fios-router.home>
2020-07-20 14:33:53 -0400

    set up basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./App.css";
 import SearchPage from "./components/SearchPage";
+import { Router, Link } from "@reach/router";
+import ResultPage from "./components/ResultPage";
 
 function App() {
   return (
     <div className="App">
-      <div className="logo">Bookstore</div>
-      <SearchPage />
+      <Link to="/">
+        <div className="logo">Bookstore</div>
+      </Link>
+      <Router>
+        <SearchPage path="/" />
+        <ResultPage path="/results" />
+      </Router>
     </div>
   );
 }
--- src/components/SearchPage/index.js
+++ src/components/SearchPage/index.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { navigate } from "@reach/router";
 
 const SearchPage = () => {
   return (
     <div>
       <h1>What book would you like to find? </h1>
-      <input type="text" placeholder="Search by book title or author name" />
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          navigate("/results");
+        }}
+      >
+        <input type="text" placeholder="Search by book title or author name" />
+        <input type="submit" />
+      </form>
     </div>
   );
 };
