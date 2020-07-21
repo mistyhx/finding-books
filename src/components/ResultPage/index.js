910165e678912981beb5f7ad47b3cdb5bbdf6d57
xin.hu <xin.hu@xinhus-MBP.fios-router.home>
2020-07-21 12:11:42 -0400

    connected with google API

--- src/components/ResultPage/index.js
+++ src/components/ResultPage/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const ResultPage = () => {
-  return <div>Results: 1 - 5</div>;
+const ResultPage = ({ location }) => {
+  const [input, setInput] = useState(location.state.searchTerm);
+  const [results, setResults] = useState("");
+  const API_URL = `https://www.googleapis.com/books/v1/volumes`;
+  const fetchBooks = async searchTerm => {
+    const response = await axios.get(`${API_URL}?q=${searchTerm}`);
+    setResults(response.data.items);
+  };
+
+  useEffect(() => {
+    fetchBooks(location.state.searchTerm);
+  }, []);
+
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          fetchBooks(input);
+        }}
+      >
+        <input type="text" placeholder="search" value={input} onChange={e => setInput(e.target.value)} />
+        <input type="submit" />
+      </form>
+      <div>{results && results.map(result => <li key={result.id}>{result.volumeInfo.title}</li>)}</div>
+      Results: 1 - 5
+    </div>
+  );
 };
 
 export default ResultPage;
--- src/components/SearchPage/index.js
+++ src/components/SearchPage/index.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { navigate } from "@reach/router";
 
 const SearchPage = () => {
+  const [term, setTerm] = useState("");
   return (
     <div>
       <h1>What book would you like to find? </h1>
       <form
         onSubmit={e => {
           e.preventDefault();
-          navigate("/results");
+          navigate("/results", { state: { searchTerm: term } });
         }}
       >
-        <input type="text" placeholder="Search by book title or author name" />
+        <input
+          type="text"
+          placeholder="Search by book title or author name"
+          value={term}
+          onChange={e => setTerm(e.target.value)}
+        />
         <input type="submit" />
       </form>
     </div>
