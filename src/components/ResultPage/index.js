8f1415551736b440a5cc2312996fa56bf97ca937
xin.hu <xin.hu@xinhus-MBP.fios-router.home>
2020-07-22 12:22:12 -0400

    added basic web layout for results page

--- src/components/ResultPage/index.js
+++ src/components/ResultPage/index.js
@@ -1,5 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Search, ChevronDown } from "react-feather";
+import "./index.css";
+
+const Book = ({ data }) => {
+  return (
+    <div className="book">
+      <div className="book-cover">
+        <a href={data.previewLink} target="_blank">
+          <img src={data.imageLinks.smallThumbnail} />
+        </a>
+      </div>
+      <div className="book-info">
+        <span className="title">{data.title}</span>
+        <div>
+          <p className="description">{data.description}</p>
+          <div className="meta">
+            <span> - {data.publishedDate}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const ResultPage = ({ location }) => {
   const [input, setInput] = useState(location.state.searchTerm);
@@ -15,19 +38,36 @@ const ResultPage = ({ location }) => {
   }, []);
 
   return (
-    <div>
-      <div>
+    <div className="results-page">
+      <div className="controls">
+        <div className="filters">
+          <div>
+            Sort by date
+            <ChevronDown size={16} />
+          </div>
+          <div>
+            Publish date <ChevronDown size={16} />
+          </div>
+          <div>
+            Categories <ChevronDown size={16} />
+          </div>
+        </div>
         <form
+          className="search-form"
           onSubmit={e => {
             e.preventDefault();
             fetchBooks(input);
           }}
         >
+          {/*<Search className="search-icon" size={24} />*/}
           <input type="text" placeholder="search" value={input} onChange={e => setInput(e.target.value)} />
-          <input type="submit" />
+          <input type="submit" value="Search" />
         </form>
       </div>
-      <div>{results && results.map(result => <li key={result.id}>{result.volumeInfo.title}</li>)}</div>;
+      <div className="results">
+        {results && results.map(result => <Book key={result.id} data={result.volumeInfo} />)}
+        {console.log(results)}
+      </div>
     </div>
   );
 };
