ae10d3a572729e9761dd77b058ae818c9aafef23
xin.hu <xin.hu@xinhus-MBP.fios-router.home>
2020-07-22 10:58:36 -0400

    stylized the form

--- src/components/SearchPage/index.js
+++ src/components/SearchPage/index.js
@@ -1,25 +1,31 @@
 import React, { useState } from "react";
 import { navigate } from "@reach/router";
+import "./index.css";
 
 const SearchPage = () => {
   const [term, setTerm] = useState("");
   return (
     <div className="search-page">
-      <h1>What book would you like to find? </h1>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          navigate("/results", { state: { searchTerm: term } });
-        }}
-      >
-        <input
-          type="text"
-          placeholder="Search by book title or author name"
-          value={term}
-          onChange={e => setTerm(e.target.value)}
-        />
-        <input type="submit" />
-      </form>
+      <div className="search-elements">
+        <h1>
+          What book would you like to <span className="highlight">find</span>?
+        </h1>
+        <form
+          className="search-form"
+          onSubmit={e => {
+            e.preventDefault();
+            navigate("/results", { state: { searchTerm: term } });
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Search by book title or author name"
+            value={term}
+            onChange={e => setTerm(e.target.value)}
+          />
+          <input type="submit" value="search" />
+        </form>
+      </div>
     </div>
   );
 };
