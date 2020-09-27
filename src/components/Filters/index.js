efe1162d20e6a1a2fd39ecef795cc9bb2a177c01
xin hu <xinhu@xins-MBP.fios-router.home>
2020-09-27 17:57:40 -0400

    added filter ui

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { ChevronDown } from "react-feather";
-import "./index.scss"
+import "./index.scss";
+
+const sorting = ["relevance", "newest"];
+const filtering = ["partial", "full", "free-ebooks", "paid-ebooks", "ebooks"];
+const printType = ["all", "books", "magazines"];
+
+const FilterMenu = ({ options }) => {
+  return (
+    <div className="filters-menu">
+      <div className="filters-menu-selected">
+        <span>By relevance</span>
+        <ChevronDown size={16} />
+      </div>
+      <div className="filters-menu-options">
+        {options.map((item, index) => (
+          <div key={index}>{item}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const Filters = () => {
   return (
     <div className="filters">
-      <div>
-        <span>Sort by relevance</span>
-        <ChevronDown size={16} />
-      </div>
-      <div>
-        <span>Publish date </span>
-        <ChevronDown size={16} />
-      </div>
-      <div>
-        <span> Categories </span>
-        <ChevronDown size={16} />
-      </div>
+      <FilterMenu type="sorting" options={sorting} />
+      <FilterMenu type="filtering" options={filtering} />
+      <FilterMenu type="print" options={printType} />
     </div>
   );
 };
