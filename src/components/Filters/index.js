3d90b14051943bb18346aa86366fced8ff185ecc
xin hu <xinhu@xins-MBP.fios-router.home>
2020-09-27 18:12:55 -0400

    added local state change for the filters

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -3,19 +3,46 @@ import { ChevronDown } from "react-feather";
 import "./index.scss";
 
 const sorting = ["relevance", "newest"];
-const filtering = ["partial", "full", "free-ebooks", "paid-ebooks", "ebooks"];
 const printType = ["all", "books", "magazines"];
 
-const FilterMenu = ({ options }) => {
+const FilterMenu = ({ type, options }) => {
+  const initialFilters = {
+    sorting: "relevance",
+    type: "all",
+  };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "UPDATE_SORT":
+        return {
+          sorting: action.payload,
+          type: state.type,
+        };
+
+      case "UPDATE_TYPE":
+        return {
+          sorting: state.sorting,
+          type: action.payload,
+        };
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialFilters);
+
   return (
     <div className="filters-menu">
       <div className="filters-menu-selected">
-        <span>By relevance</span>
+        <span>{type === "sorting" ? `By ${state.sorting}` : state.type}</span>
         <ChevronDown size={16} />
       </div>
       <div className="filters-menu-options">
         {options.map((item, index) => (
-          <div key={index}>{item}</div>
+          <div
+            key={index}
+            onClick={() => dispatch({ type: type === "sorting" ? "UPDATE_SORT" : "UPDATE_TYPE", payload: item })}
+          >
+            {item}
+          </div>
         ))}
       </div>
     </div>
@@ -26,7 +53,6 @@ const Filters = () => {
   return (
     <div className="filters">
       <FilterMenu type="sorting" options={sorting} />
-      <FilterMenu type="filtering" options={filtering} />
       <FilterMenu type="print" options={printType} />
     </div>
   );
