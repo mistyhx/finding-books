d2a773023889314d69af000cbd4d2b741ce76376
xin hu <xinhu@xins-MBP.fios-router.home>
2020-10-07 20:34:03 -0400

    added front page motion

--- src/assets/ReadingBg/index.js
+++ src/assets/ReadingBg/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.scss";
 
 const ReadingBg = () => (
   <svg id="person" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1400 957" width="2000">
--- src/components/SearchPage/index.js
+++ src/components/SearchPage/index.js
@@ -1,37 +1,71 @@
 import React, { useState } from "react";
 import { navigate } from "@reach/router";
-import backgroundImage from "../../bg.png";
+import { animated } from "react-spring";
+import { Transition } from "react-spring/renderprops";
 import "./index.scss";
 import ReadingBg from "../../assets/ReadingBg";
 
-const SearchPage = () => {
+const SearchForm = () => {
   const [term, setTerm] = useState("");
+  return (
+    <form
+      className="search-form"
+      onSubmit={e => {
+        e.preventDefault();
+        if (term) {
+          navigate("/results", { state: { searchTerm: term } });
+        }
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Search by book title and author name"
+        value={term}
+        onChange={e => setTerm(e.target.value)}
+      />
+      <input type="submit" value="search" />
+    </form>
+  );
+};
+
+const Title = () => {
+  return (
+    <h1>
+      What book would you like to <span className="highlight">find</span>?
+    </h1>
+  );
+};
+
+const Elements = [
+  props => (
+    <animated.div style={props}>
+      <Title />
+    </animated.div>
+  ),
+  props => (
+    <animated.div style={props}>
+      <SearchForm />
+    </animated.div>
+  ),
+];
+
+const SearchPage = () => {
   return (
     <div className="search">
       <div className="search-bg">
         <ReadingBg />
       </div>
       <div className="search-elements">
-        <h1>
-          What book would you like to <span className="highlight">find</span>?
-        </h1>
-        <form
-          className="search-form"
-          onSubmit={e => {
-            e.preventDefault();
-            if (term) {
-              navigate("/results", { state: { searchTerm: term } });
-            }
-          }}
+        <Transition
+          items={[0, 1]}
+          keys={(item, index) => index}
+          from={{ opacity: 0, transform: `translateY(40px)` }}
+          enter={{ opacity: 1, transform: `translateY(0px)` }}
+          leave={{ opacity: 0, transform: `translateY(-40px)` }}
+          trail={300}
         >
-          <input
-            type="text"
-            placeholder="Search by book title and author name"
-            value={term}
-            onChange={e => setTerm(e.target.value)}
-          />
-          <input type="submit" value="search" />
-        </form>
+          {item => Elements[item]}
+        </Transition>
       </div>
     </div>
   );
